b6e80a6196e36717cb8fa5bc60369be7fd7d2383
Gustavo Figueiredo <gustavodsf1@gmail.com>
2021-11-11 19:46:31 -0300

    feat: add solution

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,40 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if (newTaskTitle.trim() === '') {
+      return;
+    }
+
+    const id = Math.floor(Math.random() * 99999) + 1;
+    const newTask = {
+      id,
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {
+      return;
+    }
+
+    task.isComplete = !task.isComplete;
+
+    setTasks([...tasks]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks);
+
   }
 
   return (
